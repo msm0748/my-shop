4795b98b5eec0112efa082078d982eb0100ae67f
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2022-09-08 11:17:24 +0900

    코드 정리 및 회원가입 시 user 데이터베이스 전송

--- pages/api/kakao.js
+++ pages/api/kakao.js
@@ -1,22 +1,21 @@
 import axios from "axios";
-
-// import createFirebaseToken from "./kakaoLoginServer.js";
-// import necessary modules
-const request = require("request-promise");
-
 // Firebase setup
-const firebaseAdmin = require("firebase-admin");
+import firebaseAdmin from "firebase-admin";
 // you should manually put your service-account.json in the same folder app.js
 // is located at.
-const serviceAccount = require("./test-988dd-firebase-adminsdk-51eus-b410abd9c2.json");
+import serviceAccount from "./test-988dd-firebase-adminsdk-51eus-b410abd9c2.json";
+import { db } from "../../fBase.js";
+import { addDoc, collection } from "firebase/firestore";
 
 // Kakao API request url to retrieve user profile based on access token
 const requestMeUrl = "https://kapi.kakao.com/v2/user/me?secure_resource=true";
 
 // Initialize FirebaseApp with service-account.json
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount),
-});
+if (!firebaseAdmin.apps.length) {
+    firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(serviceAccount),
+    });
+}
 
 /**
  * requestMe - Returns user profile from Kakao API
@@ -25,12 +24,12 @@ firebaseAdmin.initializeApp({
  * @return {Promiise<Response>}      User profile response in a promise
  */
 function requestMe(kakaoAccessToken) {
-  console.log("Requesting user profile from Kakao API server.");
-  return request({
-    method: "GET",
-    headers: { Authorization: "Bearer " + kakaoAccessToken },
-    url: requestMeUrl,
-  });
+    console.log("Requesting user profile from Kakao API server.");
+    return axios({
+        method: "GET",
+        headers: { Authorization: "Bearer " + kakaoAccessToken },
+        url: requestMeUrl,
+    });
 }
 
 /**
@@ -44,33 +43,40 @@ function requestMe(kakaoAccessToken) {
  * @return {Prommise<UserRecord>} Firebase user record in a promise
  */
 function updateOrCreateUser(userId, email, displayName, photoURL) {
-  console.log("updating or creating a firebase user");
-  const updateParams = {
-    provider: "KAKAO",
-    displayName: displayName,
-  };
-  if (displayName) {
-    updateParams["displayName"] = displayName;
-  } else {
-    updateParams["displayName"] = email;
-  }
-  if (photoURL) {
-    updateParams["photoURL"] = photoURL;
-  }
-  console.log(updateParams, "updateParams");
-  return firebaseAdmin
-    .auth()
-    .updateUser(userId, updateParams)
-    .catch((error) => {
-      if (error.code === "auth/user-not-found") {
-        updateParams["uid"] = userId;
-        if (email) {
-          updateParams["email"] = email;
-        }
-        return firebaseAdmin.auth().createUser(updateParams);
-      }
-      throw error;
-    });
+    console.log("updating or creating a firebase user");
+    const updateParams = {
+        provider: "KAKAO",
+        displayName: displayName,
+    };
+    if (displayName) {
+        updateParams["displayName"] = displayName;
+    } else {
+        updateParams["displayName"] = email;
+    }
+    if (photoURL) {
+        updateParams["photoURL"] = photoURL;
+    }
+    console.log(updateParams, "updateParams");
+    return firebaseAdmin
+        .auth()
+        .updateUser(userId, updateParams)
+        .catch(async (error) => {
+            if (error.code === "auth/user-not-found") {
+                updateParams["uid"] = userId;
+                if (email) {
+                    updateParams["email"] = email;
+                }
+                await addDoc(collection(db, "user"), {
+                    name: updateParams.displayName,
+                    email: updateParams.email,
+                    photoURL: updateParams.photoURL,
+                    uid: updateParams.uid,
+                    createdAt: Date.now(),
+                });
+                return firebaseAdmin.auth().createUser(updateParams); // 신규 유저 생성
+            }
+            throw error;
+        });
 }
 
 /**
@@ -79,36 +85,46 @@ function updateOrCreateUser(userId, email, displayName, photoURL) {
  * @param  {String} kakaoAccessToken access token from Kakao Login API
  * @return {Promise<String>}                  Firebase token in a promise
  */
-function createFirebaseToken(kakaoAccessToken) {
-  return requestMe(kakaoAccessToken)
-    .then((response) => {
-      const body = JSON.parse(response);
-      console.log(body, "body");
-      const userId = `kakao:${body.id}`;
-      if (!userId) {
-        return res.status(404).send({ message: "There was no user with the given access token." });
-      }
-      let nickname = null;
-      let profileImage = null;
-      if (body.properties) {
-        nickname = body.properties.nickname;
-        profileImage = body.properties.profile_image;
-      }
-      return updateOrCreateUser(userId, body.kakao_account.email, nickname, profileImage);
-    })
-    .then((userRecord) => {
-      const userId = userRecord.uid;
-      console.log(`creating a custom firebase token based on uid ${userId}`);
-      return firebaseAdmin.auth().createCustomToken(userId, { provider: "KAKAO" });
-    });
+async function createFirebaseToken(kakaoAccessToken) {
+    return await requestMe(kakaoAccessToken)
+        .then((response) => {
+            const body = response.data;
+            const userId = `kakao:${body.id}`;
+            if (!userId) {
+                return res.status(404).send({
+                    message: "There was no user with the given access token.",
+                });
+            }
+            let nickname = null;
+            let profileImage = null;
+            if (body.properties) {
+                nickname = body.properties.nickname;
+                profileImage = body.properties.profile_image;
+            }
+            return updateOrCreateUser(
+                userId,
+                body.kakao_account.email,
+                nickname,
+                profileImage
+            );
+        })
+        .then((userRecord) => {
+            const userId = userRecord.uid;
+            console.log(
+                `creating a custom firebase token based on uid ${userId}`
+            );
+            return firebaseAdmin
+                .auth()
+                .createCustomToken(userId, { provider: "KAKAO" });
+        });
 }
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { access_token } = req.body;
-    const result = await createFirebaseToken(access_token);
-    res.status(200).json({
-      result,
-    });
-  }
+    if (req.method === "POST") {
+        const { access_token } = req.body;
+        const result = await createFirebaseToken(access_token);
+        return res.status(200).json({
+            result,
+        });
+    }
 }
