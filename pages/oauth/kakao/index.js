e24bd10e5ef7caf488f54cf61020d22ecf05607b
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2022-09-08 11:19:20 +0900

    코드 정리

--- pages/oauth/kakao/index.js
+++ pages/oauth/kakao/index.js
@@ -1,48 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { signInWithCustomToken } from "firebase/auth";
 import { auth } from "../../../fBase";
 
 export default function Kakao() {
-  const router = useRouter();
-  useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get("code");
-    getToken(code);
-  }, []);
-  const getToken = async (code) => {
-    const {
-      data: { access_token },
-    } = await axios({
-      url: `https://kauth.kakao.com/oauth/token`,
-      method: "post",
-      params: {
-        grant_type: "authorization_code",
-        client_id: "74a4303eee0b4f4a236a6e528ab42fbe",
-        redirect_uri: "http://localhost:3000/oauth/kakao",
-        code: code,
-      },
-    });
-    const {
-      data: { result },
-    } = await axios({
-      url: "/api/kakao",
-      method: "post",
-      data: { access_token: access_token },
-      withCredentials: true,
-    }).then((res) => {
-      return res;
-    });
+    const router = useRouter();
+    useEffect(() => {
+        const code = new URL(window.location.href).searchParams.get("code");
+        getToken(code);
+    }, []);
+    const getToken = async (code) => {
+        const {
+            data: { access_token },
+        } = await axios({
+            url: `https://kauth.kakao.com/oauth/token`,
+            method: "post",
+            params: {
+                grant_type: "authorization_code",
+                client_id: "74a4303eee0b4f4a236a6e528ab42fbe",
+                redirect_uri: "http://localhost:3000/oauth/kakao",
+                code: code,
+            },
+        });
+        const {
+            data: { result },
+        } = await axios({
+            url: "/api/kakao",
+            method: "post",
+            data: { access_token: access_token },
+            withCredentials: true,
+        });
 
-    signInWithCustomToken(auth, result)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
-  };
-  return <div>카카오</div>;
+        signInWithCustomToken(auth, result)
+            .then(() => {
+                router.push("/");
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                alert(errorCode);
+            });
+    };
 }
