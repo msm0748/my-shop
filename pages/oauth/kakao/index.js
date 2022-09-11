d6442598db524401bac6f1e9f5697674f461f2b9
Moon-Seokmin <msm0748@gmail.com>
2022-09-12 02:39:25 +0900

    err 수정

--- pages/oauth/kakao/index.js
+++ pages/oauth/kakao/index.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 import { useEffect, useCallback } from "react";
-=======
-import { useEffect, useState } from "react";
->>>>>>> 1c5be0c63f543e4f521b68feab6ecf75e3824d39
 import axios from "axios";
 import { useRouter } from "next/router";
 import { signInWithCustomToken } from "firebase/auth";
 import { auth } from "../../../fBase";
 import Loading from "../../../components/Template/Loding";
 
-<<<<<<< HEAD
 export default function KakaoAuth() {
   const router = useRouter();
   useEffect(() => {
@@ -62,43 +57,4 @@ export default function KakaoAuth() {
       <Loading />
     </div>
   );
-=======
-export default function Kakao() {
-    const router = useRouter();
-    useEffect(() => {
-        const code = new URL(window.location.href).searchParams.get("code");
-        getToken(code);
-    }, []);
-    const getToken = async (code) => {
-        const {
-            data: { access_token },
-        } = await axios({
-            url: `https://kauth.kakao.com/oauth/token`,
-            method: "post",
-            params: {
-                grant_type: "authorization_code",
-                client_id: "74a4303eee0b4f4a236a6e528ab42fbe",
-                redirect_uri: "https://my-shop-brown.vercel.app/oauth/kakao",
-                code: code,
-            },
-        });
-        const {
-            data: { result },
-        } = await axios({
-            url: "/api/kakao",
-            method: "post",
-            data: { access_token: access_token },
-            withCredentials: true,
-        });
-
-        signInWithCustomToken(auth, result)
-            .then(() => {
-                router.push("/");
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                alert(errorCode);
-            });
-    };
->>>>>>> 1c5be0c63f543e4f521b68feab6ecf75e3824d39
 }
